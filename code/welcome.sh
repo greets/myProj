bd8ae103e6a8a424063de4bdc481da54d58ad3b0
divya <dev@developer.com>
2015-03-05 12:05:37 +0530

    welcome.sh file modified

--- code/welcome.sh
+++ code/welcome.sh
@@ -8,8 +8,11 @@ echo "TimeStamp: " ` date "+%Y_%m_%d_%H:%M:%S" `
 #today 
 echo "Hello $LOGNAME !"
 echo "Changes committed"
+<<<<<<< HEAD
 echo "changes staged"
 echo "changes only in working dir"
+=======
+>>>>>>> 36b2f38... commit changes to welcome.sh
 }
 
 main $@
